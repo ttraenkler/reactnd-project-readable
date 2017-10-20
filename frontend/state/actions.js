903da1cc97feb49682c8d8f70c6c4fd8efeaebc1
Thomas Tränkler <git@thomas.traenkler.com>
2017-10-20 13:09:48 +0200

    WIP: redux

---
+++ frontend/state/actions.js
@@ -0,0 +1,14 @@
+const types = {
+  comment: {
+    create: "create comment",
+    edit: "edit comment",
+    remove: "remove comment"
+  },
+  post: {
+    create: "create post",
+    edit: "edit post",
+    remove: "remove post"
+  }
+};
+
+export default actions;
